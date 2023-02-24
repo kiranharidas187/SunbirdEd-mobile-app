2e423ff00f153ce64c539be3d6ea3fd5399d3faa
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2023-02-24 16:44:19 +0530

    mentor details changes

--- src/app/mentoring/mentoring.module.ts
+++ src/app/mentoring/mentoring.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MentoringHomeComponent } from './mentoring-home/mentoring-home.component';
 import { MentorCardComponent } from './components/mentor-card/mentor-card.component';
 import { MentorDetailsComponent } from './components/mentor-card/mentor-details/mentor-details.component';
+import { MentorSessionCardComponent } from './components/mentor-card/mentor-session-card/mentor-session-card.component';
 
 const routes: Routes = [
   {
@@ -20,7 +21,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [MentoringHomeComponent, MentorCardComponent, MentorDetailsComponent],
+  declarations: [MentoringHomeComponent, MentorCardComponent, MentorDetailsComponent, MentorSessionCardComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
